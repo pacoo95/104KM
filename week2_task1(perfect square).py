138a9b8035b859e3769070b39241d4517de2986a
Plamen Atanasov <atanaso4@uni.coventry.ac.uk>
2016-11-13 18:17:17 +0000

    Add files via upload
    Task1 from week 2 for coursework. Python function which returns the highest perfect square.

---
+++ week2_task1(perfect square).py
@@ -0,0 +1,14 @@
+'''A function which returns the highest perfect square which is less
+    or equal to its parameter (a positive integer).'''
+
+integer = int(input("Please enter an integer: "))
+def perfectsquare(integer):
+    i = 1
+    while True:             #iterates while the condition is True
+        if i*i > integer:   #checks if i multiplied by itself is higher than the input
+            break
+        i +=1               #if i multiplied by itslef isn't higher than the input increases i
+
+    i = i - 1
+    print(i*i)
+perfectsquare(integer)
