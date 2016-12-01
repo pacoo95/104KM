f043d69612b57142b3014bc717bac2337b5c4c53
Plamen Atanasov <atanaso4@uni.coventry.ac.uk>
2016-12-01 21:25:52 +0000

    task 3 - pseudocode

---
+++ task3 (pseudocode).py
@@ -0,0 +1,10 @@
+HIGHEST_PERFECT_SQUARE()
+    INPUT <- (integer)          
+    i <- 1              
+    while True                  
+        if i*i > integer
+            break
+        i <- i+1
+    
+    i <- i-1
+    return (i*i)
