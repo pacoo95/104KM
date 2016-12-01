be52b4aa2b60f5c0f9dc1ebfb6ae4451bdc15bbd
Plamen Atanasov <atanaso4@uni.coventry.ac.uk>
2016-12-01 09:28:17 +0000

    Update week2_task1(perfect square).py

--- week2_task1(perfect square).py
+++ week2_task1(perfect square).py
@@ -1,8 +1,9 @@
-'''A function which returns the highest perfect square which is less
-    or equal to its parameter (a positive integer).'''
+
 
 integer = int(input("Please enter an integer: "))
 def perfectsquare(integer):
+    '''A function which returns the highest perfect square which is less
+    or equal to its parameter (a positive integer).'''
     i = 1
     while True:             #iterates while the condition is True
         if i*i > integer:   #checks if i multiplied by itself is higher than the input
