a7ff9ccf4f5b1454ddbe13b78c8fbedd3645b273
Plamen Atanasov <atanaso4@uni.coventry.ac.uk>
2016-12-01 09:48:37 +0000

    task5 (pseudocode)

---
+++ task5 (pseudocode).py
@@ -0,0 +1,43 @@
+'''Pseducodoe for functions for addition,
+    subtraction and multiplication of two  matrices of order n'''
+
+INPUT(Matrix)
+    B <- INPUT Matrix
+    C <- INPUT Matrix
+
+ADD(B,C)
+    for i<-0 to n                    #n
+        for j<-0 to n               #n*n
+            A[i][j] <- B[i][j]+C[i][j]
+
+    return A
+
+MULTIPYCONSTANT(A, p)
+    for i<-0 to n                #n
+        for j<-0 to n            #n*n
+            A[i][j] <- ADD[i][j]*P
+            
+    return A
+
+MULTIPLYMATRIX(B,C)
+    for i<-0 to n                #n
+        for j<-0 to n            #n*n
+            for k<-0 to n        #n*n*n
+                value <-  B[i,k]*C[k,j]
+                res[i][j] <-res[i][j] + value
+    return res
+
+SUBTRACT(B,C)
+    for i<-0 to n                #n
+        for j<-0 to n            #n*n
+            A[i][j] <- B[i][j] - C[i][j]
+
+    return A
+
+R <- B*C + 2*(B+C)
+R1 <- MULTIPLYMATRIX(B,C)
+R2 <- ADD(B,C)
+R3 <- SUBTRACT(B,C)
+R4 <- MULTIPLYCONSTANT(A,2)
+
+#Big O Notation = n*n*n
