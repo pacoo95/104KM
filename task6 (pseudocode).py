99e84c19bdcf5b99e7b9717ad00020492fc48573
Plamen Atanasov <atanaso4@uni.coventry.ac.uk>
2016-12-01 21:26:35 +0000

    task 6 - pseudocode

---
+++ task6 (pseudocode).py
@@ -0,0 +1,9 @@
+INPUT <- (sentence)
+
+REVERSESENTENCE(sentence)
+    if length of sentence<- 0
+        return word
+    else
+        return reversesentence(sentence from 1st word onwards)+[the first word]
+    
+print(reversesentence(splitted sentence)
